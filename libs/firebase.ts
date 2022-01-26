653f8ba19c2ac2da90c742c4d4412f5829d0d87a
BojanSibar <bojan.sibar@gmail.com>
2022-01-26 09:18:33 -0500

    add vars to env file

--- libs/firebase.ts
+++ libs/firebase.ts
@@ -4,14 +4,16 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_API_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
+
+console.log(process.env.FIREBASE_MEASUREMENT_ID);
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
